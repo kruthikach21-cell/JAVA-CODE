335d42dff11e8910d9a68743701b8156ddf965c3
kruthika21 <welcome@DESKTOP-A2R2471>
2025-09-20 14:47:16 +0530

    Sample

---
+++ Java/src/com/tnsif/oops/ClassDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.oops;
+
+public class ClassDemo { 
+	public static void main(String[] args) {
+	Sample s=new Sample();
+	System.out.println(s.a);
+	System.out.println(s.b);
+	s.show();
+
+}
+
+}
+
+
---
+++ Java/src/com/tnsif/oops/Sample.java
@@ -0,0 +1,11 @@
+package com.tnsif.oops;
+
+public class Sample {int a=10;
+boolean b=true;
+public void show() {
+	System.out.println("This is a show method");
+}
+
+}
+
+
