cf31aca04639393ab28c09bda9bb8d9f33a88704
kruthika21 <welcome@DESKTOP-A2R2471>
2025-09-20 14:26:34 +0530

    Ifelseifelse

---
+++ Java/src/com/tnsif/conditionals/Ifelseifelse.java
@@ -0,0 +1,20 @@
+package com.tnsif.conditionals;
+
+public class Ifelseifelse {
+	public static void main(String[] args) {
+	int a=15,b=9,c=21;
+	if(a>b&&a>c) {
+		System.out.println("A is greater");
+	}
+	else if(b>a&&b>c) {
+		System.out.println("B is greater");
+	}
+	else {
+		System.out.println("C is greater");
+	}
+
+}
+
+}
+
+
