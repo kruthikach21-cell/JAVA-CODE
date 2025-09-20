1b41ea6c82c2c0e63cf02cba0e30416d4d1bd9d9
kruthika21 <welcome@DESKTOP-A2R2471>
2025-09-20 12:11:34 +0530

    palindrome

---
+++ Palindrome/src/Palindrome/Palindrome.java
@@ -0,0 +1,24 @@
+package Palindrome;
+
+public class Palindrome {
+	public static void main(String[] args) {
+		int num = 121;  
+        int temp = num;
+        int rev = 0;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            rev = rev * 10 + digit;
+            temp = temp / 10;
+        }
+
+        if (num == rev) {
+            System.out.println(num + " is a Palindrome.");
+        } else {
+        	  System.out.println(num + " is NOT a Palindrome.");
+
+	}
+
+	}
+
+}
