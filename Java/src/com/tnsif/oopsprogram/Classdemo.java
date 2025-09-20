d5d7fb857915f74babfb7850aa30d4419da5ef35
kruthika21 <welcome@DESKTOP-A2R2471>
2025-09-20 22:49:23 +0530

    Classdemo

---
+++ Java/src/com/tnsif/oopsprogram/Classdemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.oopsprogram;
+
+
+public class Classdemo extends Sample {
+	public static void main(String[] args) {
+		Sample s=new Sample();
+		System.out.println(s.a);
+		System.out.println(s.b);
+		s.show();
+
+}
+}
+
