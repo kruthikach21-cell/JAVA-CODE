ff99831301cc80a180afd2079bab125f0284eeb5
kruthika21 <welcome@DESKTOP-A2R2471>
2025-09-20 14:15:16 +0530

    Ifconditions

---
+++ Java/src/com/tnsif/conditionals/Ifcondition.java
@@ -0,0 +1,13 @@
+package com.tnsif.conditionals;
+
+public class Ifcondition {public static void main(String[] args) {
+	int num=12;
+	if(num>10) {
+		System.out.println("Number is Greater than 10");
+	}
+
+}
+
+}
+
+
