cf81a9751129bfd812c995acb586579e35f04cde
kruthika21 <welcome@DESKTOP-A2R2471>
2025-09-20 22:47:53 +0530

    Sample

---
+++ Java/src/com/tnsif/oopsprogram/Sample.java
@@ -0,0 +1,14 @@
+package com.tnsif.oopsprogram;
+
+
+public class Sample {
+		int a=20;
+		boolean b=true;
+		public void show() {
+			System.out.println("This is a show method");
+		}
+		
+	}
+
+
+
