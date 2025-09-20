503267f04f7fe4edb68de76534b92dc117615365
kruthika21 <welcome@DESKTOP-A2R2471>
2025-09-20 14:55:46 +0530

    Sample

--- Java/src/com/tnsif/oops/ClassDemo.java
+++ Java/src/com/tnsif/oops/ClassDemo.java
@@ -1,14 +1,13 @@
 package com.tnsif.oops;
 
-public class ClassDemo { 
+public class ClassDemo {
 	public static void main(String[] args) {
-	Sample s=new Sample();
-	System.out.println(s.a);
-	System.out.println(s.b);
-	s.show();
+		Sample s=new Sample();
+		System.out.println(s.a);
+		System.out.println(s.b);
+		s.show();
+
+	}
 
 }
-
-}
-
 
