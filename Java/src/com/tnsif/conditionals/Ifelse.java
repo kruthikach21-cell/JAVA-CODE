64dd864a46a194ecf0716cb169a8ae947fcf4e1b
kruthika21 <welcome@DESKTOP-A2R2471>
2025-09-20 14:20:37 +0530

    Ifelse

---
+++ Java/src/com/tnsif/conditionals/Ifelse.java
@@ -0,0 +1,17 @@
+package com.tnsif.conditionals;
+
+public class Ifelse {
+	public static void main(String[] args) {
+	int age=18;
+	if(age>=18) {
+		System.out.println("Major");
+	}
+	else {
+		System.out.println("Minor");
+	}
+
+}
+
+
+
+}
