c60c459c9f815b8a1f51a9aba9bf38559c488ef6
kruthika21 <welcome@DESKTOP-A2R2471>
2025-09-20 22:08:13 +0530

    HierarchicalVehicle

---
+++ Java/src/com/tnsif/oops/HierarchicalVehicle.java
@@ -0,0 +1,52 @@
+package com.tnsif.oops;
+
+
+	class Vehicle {
+    void start() {
+        System.out.println("Vehicle is starting...");
+    }
+}
+
+class Car extends Vehicle {
+    void drive() {
+        System.out.println("Car is driving...");
+    }
+}
+
+class Bike extends Vehicle {
+    void ride() {
+        System.out.println("Bike is riding...");
+    }
+}
+
+class Truck extends Vehicle {
+    void load() {
+        System.out.println("Truck is loading goods...");
+    }
+}
+
+public class HierarchicalVehicle {
+
+    public static void main(String[] args) {
+       
+        Car car = new Car();
+        car.start();   
+        car.drive();   
+
+        System.out.println();
+
+        
+        Bike bike = new Bike();
+        bike.start();
+        bike.ride();
+
+        System.out.println();
+
+       
+        Truck truck = new Truck();
+        truck.start();
+        truck.load();
+    }
+}
+
+
