e90580a13a4589545fcaeab15dc759e7c3865449
kruthika21 <welcome@DESKTOP-A2R2471>
2025-09-21 22:31:06 +0530

    ShortWrapper

---
+++ Java/src/com/tnsif/WrapperClasses/ShortWrapper.java
@@ -0,0 +1,12 @@
+package com.tnsif.WrapperClasses;
+
+public class ShortWrapper {
+	 public static void main(String[] args) {
+	        short s = 20;
+	        Short shortObj = s;
+	        short sVal = shortObj;
+
+	        System.out.println("Short object: " + shortObj);
+	        System.out.println("Primitive short: " + sVal);
+	    }
+}
