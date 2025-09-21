e82abcf43c99a0c68232ab10bb9f2d50dce88a6d
kruthika21 <welcome@DESKTOP-A2R2471>
2025-09-21 22:36:32 +0530

    DefaultConstructor

---
+++ Java/src/com/tnsif/constructors/DefaultConstructor.java
@@ -0,0 +1,19 @@
+package com.tnsif.constructors;
+class Student{
+	
+	Student(){
+		System.out.println("Default constructor is called!");
+	}
+}
+
+public class DefaultConstructor {
+
+	public static void main(String[] args) {
+		Student s=new Student();
+
+	}
+
+}
+
+
+
