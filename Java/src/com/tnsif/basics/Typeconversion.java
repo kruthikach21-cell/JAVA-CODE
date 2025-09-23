2c2e507ad02c06c427b90c0a83b6fa0bf10237d2
kruthika21 <welcome@DESKTOP-A2R2471>
2025-09-23 22:36:14 +0530

    Typeconversion

---
+++ Java/src/com/tnsif/basics/Typeconversion.java
@@ -0,0 +1,11 @@
+package com.tnsif.basics;
+
+public class Typeconversion {
+	public static void main(String[] args) {
+		int myInt = 9;
+		double myDouble = myInt;//it coverts int into double
+		System.out.println(myInt);//it prints int value
+		System.out.println(myDouble);//it prints double value which is converted
+	}
+
+}
