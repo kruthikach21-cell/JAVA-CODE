4820bcd2bfea970271a4a2de51ff93476cd85e29
kruthika21 <welcome@DESKTOP-A2R2471>
2025-09-23 22:52:07 +0530

    Typeconversion

---
+++ Java/src/com/tnsif/basics/TypeconversionNarrowing.java
@@ -0,0 +1,13 @@
+ package com.tnsif.basics;
+
+public class TypeconversionNarrowing {
+	public static void main(String[] args) {
+		
+	
+	double myDouble = 9.78d;//double value
+	int myInt = (int) myDouble;//it converts double value into int
+	System.out.println(myDouble);//prints double value
+	System.out.println(myInt);//prints int value
+	
+	}
+}
