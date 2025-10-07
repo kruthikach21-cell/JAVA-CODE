198837ed5ecc18af9cc73e1ccff9b7337171feec
kruthika21 <welcome@DESKTOP-A2R2471>
2025-10-07 22:57:07 +0530

    Stringbuffer

---
+++ Java/src/com/tnsif/String/Stringbuffer.java
@@ -0,0 +1,12 @@
+package com.tnsif.String;
+
+public class Stringbuffer {
+
+	public static void main(String[] args) {
+		StringBuffer sb=new StringBuffer("tkr");
+		System.out.println(sb);
+		System.out.println(sb.append("Hello"));
+		System.out.println(sb);
+	}
+
+}
